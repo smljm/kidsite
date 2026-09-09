6c1e9cf83cc2dba8c07998ffecef0d094bca2847
ljm <dlwlsahdlqslek101110rnrmf@gmail.com>
2026-09-09 03:31:53 +0000

    ADi오후 12:312026. 9. 9.2523: {{9PM오후 12:31e}}

--- a.py
+++ a.py
@@ -1,19 +1,3 @@
-def Add_andMultiply(a, b):
-    s = a + b
-    m = a * b
-    return s, m
-
-d1, d2 = Add_andMultiply(5,9)
-print(d1, d2)
-
-def gugudan(a=2):
-    for b in range(1, 10):
-        print("%d x %d = %2d" % (a, b, a * b))
-    return 
-
-gugudan()
-gugudan(7)
-
 def gugudan_grid(start, end):
     dans = list(range(start, end + 1))
     for start in range(0, len(dans), 4):
@@ -24,3 +8,12 @@ def gugudan_grid(start, end):
                 line += "%d x %d = %2d\t" % (d, b, d * b)
             print(line)
         print()  
+
+if __name__ == "__main__":
+    num1 = int(input("첫 번째 숫자를 입력하세요: "))
+    num2 = int(input("두 번째 숫자를 입력하세요: "))
+ 
+    start = min(num1, num2)
+    end = max(num1, num2)
+ 
+    gugudan_grid(start, end)
